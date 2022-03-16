134f8d760b5cdcf57289184a970d78e4e3cb3d0e
Pedro Matos <pedrohematos@outlook.com>
2022-03-16 10:18:02 -0300

    feat: add categories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  try {
+    createCategoryService.execute({ name, description });
+    return res.status(201).send();
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.json(all);
+});
+
+export { categoriesRoutes };
