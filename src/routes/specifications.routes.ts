116406a06bdcb66d4215d52db0535968ed2f7c0d
Pedro Matos <pedrohematos@outlook.com>
2022-03-16 14:26:08 -0300

    refactor: specifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,29 +1,15 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+import { listSpecificationsController } from "../modules/cars/useCases/listSpecifications";
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  try {
-    createCategoryService.execute({ name, description });
-    return res.status(201).send();
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
+  return createSpecificationController.handle(req, res);
 });
 
 specificationsRoutes.get("/", (req, res) => {
-  const all = specificationsRepository.list();
-
-  return res.json(all);
+  return listSpecificationsController.handle(req, res);
 });
 
 export { specificationsRoutes };
